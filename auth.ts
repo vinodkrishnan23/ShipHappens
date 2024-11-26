95baf2d99fa28387d89667c84e5dfb5d9fb72be9
vinodkrishnan23 <vinod.krishnan@mongodb.com>
2024-11-26 23:59:19 +0530

    Added helm ksec import

--- auth.ts
+++ auth.ts
@@ -2,6 +2,5 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
-  trustHost: true
+  providers: [Google]
 })
