349649d18928b6b6372e080a1fedd62977bb207f
vinodkrishnan23 <vinod.krishnan@mongodb.com>
2024-11-27 07:49:19 +0530

    Added helm ksec import

--- auth.ts
+++ auth.ts
@@ -2,6 +2,12 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [Google(
+    {
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET
+  }
+)],
   trustHost: true,
+  secret: process.env.AUTH_SECRET
 })
